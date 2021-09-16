0b858566dd8655168e24376611e1766b39e3caf1
Steven Yan <examsherpa@gmail.com>
2021-09-16 10:19:55 -0400

    update for homestretch

--- Flictionary-App/image_utils.py
+++ Flictionary-App/image_utils.py
@@ -15,71 +15,71 @@ import imageio
 from PIL import Image
 from PIL import ImageOps
 
-def load_data():
-    # Check for already loaded datasets
-    if not(path.exists('xtrain_doodle.pickle')):
-        # Load from web
-        print("Loading data from the web \n")
+# def load_data():
+#     # Check for already loaded datasets
+#     if not(path.exists('xtrain_doodle.pickle')):
+#         # Load from web
+#         print("Loading data from the web \n")
 
-        # Classes we will load
-        categories = ['bee', 'cat', 'cow', 'dog', 'duck', 'horse', 'pig', 'rabbit', 'snake', 'whale']
+#         # Classes we will load
+#         categories = ['bee', 'cat', 'cow', 'dog', 'duck', 'horse', 'pig', 'rabbit', 'snake', 'whale']
 
-        # Dictionary for URL and class labels
-        URL_DATA = {}
-        for category in categories:
-            URL_DATA[category] = 'https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/' + category +'.npy'
+#         # Dictionary for URL and class labels
+#         URL_DATA = {}
+#         for category in categories:
+#             URL_DATA[category] = 'https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/' + category +'.npy'
 
-        # Load data for classes in dictionary
-        classes_dict = {}
-        for key, value in URL_DATA.items():
-            response = requests.get(value)
-            classes_dict[key] = np.load(BytesIO(response.content))
+#         # Load data for classes in dictionary
+#         classes_dict = {}
+#         for key, value in URL_DATA.items():
+#             response = requests.get(value)
+#             classes_dict[key] = np.load(BytesIO(response.content))
 
-        # Generate labels and add labels to loaded data
-        for i, (key, value) in enumerate(classes_dict.items()):
-            value = value.astype('float32')/255.
-            if i == 0:
-                classes_dict[key] = np.c_[value, np.zeros(len(value))]
-            else:
-                classes_dict[key] = np.c_[value,i*np.ones(len(value))]
+#         # Generate labels and add labels to loaded data
+#         for i, (key, value) in enumerate(classes_dict.items()):
+#             value = value.astype('float32')/255.
+#             if i == 0:
+#                 classes_dict[key] = np.c_[value, np.zeros(len(value))]
+#             else:
+#                 classes_dict[key] = np.c_[value,i*np.ones(len(value))]
 
-        # Create a dict with label codes
-        label_dict = {0:'bee', 1:'cat', 2:'cow', 3:'dog', 4:'duck',
-                      5:'horse', 6:'pig', 7:'rabbit', 8:'snake', 9:'whale'}
+#         # Create a dict with label codes
+#         label_dict = {0:'bee', 1:'cat', 2:'cow', 3:'dog', 4:'duck',
+#                       5:'horse', 6:'pig', 7:'rabbit', 8:'snake', 9:'whale'}
 
 
-        lst = []
-        for key, value in classes_dict.items():
-            lst.append(value[:3000])
-        doodles = np.concatenate(lst)
+#         lst = []
+#         for key, value in classes_dict.items():
+#             lst.append(value[:3000])
+#         doodles = np.concatenate(lst)
 
-        # Split the data into features and class labels (X & y respectively)
-        y = doodles[:,-1].astype('float32')
-        X = doodles[:,:784]
+#         # Split the data into features and class labels (X & y respectively)
+#         y = doodles[:,-1].astype('float32')
+#         X = doodles[:,:784]
 
-        # Split each dataset into train/test splits
-        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=1)
-    else:
-        # Load data from pickle files
-        print("Loading data from pickle files \n")
+#         # Split each dataset into train/test splits
+#         X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=1)
+#     else:
+#         # Load data from pickle files
+#         print("Loading data from pickle files \n")
 
-        file = open("xtrain_doodle.pickle",'rb')
-        X_train = pickle.load(file)
-        file.close()
+#         file = open("xtrain_doodle.pickle",'rb')
+#         X_train = pickle.load(file)
+#         file.close()
 
-        file = open("xtest_doodle.pickle",'rb')
-        X_test = pickle.load(file)
-        file.close()
+#         file = open("xtest_doodle.pickle",'rb')
+#         X_test = pickle.load(file)
+#         file.close()
 
-        file = open("ytrain_doodle.pickle",'rb')
-        y_train = pickle.load(file)
-        file.close()
+#         file = open("ytrain_doodle.pickle",'rb')
+#         y_train = pickle.load(file)
+#         file.close()
 
-        file = open("ytest_doodle.pickle",'rb')
-        y_test = pickle.load(file)
-        file.close()
+#         file = open("ytest_doodle.pickle",'rb')
+#         y_test = pickle.load(file)
+#         file.close()
 
-    return X_train, y_train, X_test, y_test
+#     return X_train, y_train, X_test, y_test
 
 def view_image(img, filename = 'image'):
     """
@@ -92,36 +92,36 @@ def view_image(img, filename = 'image'):
         None
     """
     fig, ax = plt.subplots(figsize=(6,9))
-    ax.imshow(img.reshape(28, 28).squeeze())
+    ax.imshow(img.reshape(80, 80).squeeze())
     ax.axis('off')
 
     plt.savefig(filename + '.png')
 
 def convert_to_PIL(img):
     """
-    Function to convert numpy (1, 784) image to PIL image.
+    Function to convert numpy (1, 6400) image to PIL image.
     INPUT:
-        img - (numpy array) image from train dataset with size (1, 784)
+        img - (numpy array) image from train dataset with size (1, 6400)
     OUTPUT:
-        pil_img - (PIL Image) 28x28 image
+        pil_img - (PIL Image) 80x80 image
     """
-    img_r = img.reshape(28,28)
+    img_r = img.reshape(80,80)
 
-    pil_img = Image.new('RGB', (28, 28), 'white')
+    pil_img = Image.new('RGB', (80, 80), 'white')
     pixels = pil_img.load()
 
-    for i in range(0, 28):
-        for j in range(0, 28):
+    for i in range(0, 80):
+        for j in range(0, 80):
             if img_r[i, j] > 0:
                 pixels[j, i] = (255 - int(img_r[i, j] * 255), 255 - int(img_r[i, j] * 255), 255 - int(img_r[i, j] * 255))
 
     return pil_img
 
-def rotate_image(src_im, angle = 45, size = (28,28)):
+def rotate_image(src_im, angle = 45, size = (80,80)):
     """
     Function to rotate PIL Image file
     INPUT:
-        src_im - (PIL Image) 28x28 image to be rotated
+        src_im - (PIL Image) image to be rotated
         angle - angle to rotate the image
         size - (tuple) size of the output image
     OUTPUT:
@@ -139,7 +139,7 @@ def flip_image(src_im):
     """
     Function to flip a PIL Image file.
     INPUT:
-        scr_im - (PIL Image) 28x28 image to be flipped
+        scr_im - (PIL Image) 80x80 image to be flipped
     OUTPUT:
         dst_im - (PIL Image) flipped image
     """
@@ -152,116 +152,116 @@ def convert_to_np(pil_img):
     INPUT:
         pil_img - (PIL Image) 28x28 image to be converted
     OUTPUT:
-        img - (numpy array) converted image with shape (28, 28)
+        img - (numpy array) converted image with shape (80, 80)
     """
     pil_img = pil_img.convert('RGB')
 
-    img = np.zeros((28, 28))
+    img = np.zeros((80, 80))
     pixels = pil_img.load()
 
-    for i in range(0, 28):
-        for j in range(0, 28):
+    for i in range(0, 80):
+        for j in range(0, 80):
             img[i, j] = 1 - pixels[j, i][0] / 255
 
     return img
 
-def add_flipped_and_rotated_images(X_train, y_train):
-    """
-    Function which adds flipped and rotated images to the original dataset.
-    INPUT:
-        X_train - (numpy array) the original training set
-        y_train - (numpy array) original labels dataset
-    OUTPUT:
-        X_Train_new - (numpy array) the dataset with added flipped and rotated
-        images
-        y_train_new - (numpy array) labels for the new training dataset
-    """
-    print("Adding flipped and rotated images to the training set. \n")
+# def add_flipped_and_rotated_images(X_train, y_train):
+#     """
+#     Function which adds flipped and rotated images to the original dataset.
+#     INPUT:
+#         X_train - (numpy array) the original training set
+#         y_train - (numpy array) original labels dataset
+#     OUTPUT:
+#         X_Train_new - (numpy array) the dataset with added flipped and rotated
+#         images
+#         y_train_new - (numpy array) labels for the new training dataset
+#     """
+#     print("Adding flipped and rotated images to the training set. \n")
 
-    X_train_new = X_train.copy()
-    y_train_new = y_train.copy().reshape(y_train.shape[0], 1)
+#     X_train_new = X_train.copy()
+#     y_train_new = y_train.copy().reshape(y_train.shape[0], 1)
 
-    for i in range(0, X_train.shape[0]):
-        # get image to rotate and flip
-        img = X_train[i]
-        pil_img = convert_to_PIL(img)
+#     for i in range(0, X_train.shape[0]):
+#         # get image to rotate and flip
+#         img = X_train[i]
+#         pil_img = convert_to_PIL(img)
 
-        # get random angle
-        angle = random.randint(5, 10)
+#         # get random angle
+#         angle = random.randint(5, 10)
 
-        # rotate and flip
-        rotated = convert_to_np(rotate_image(pil_img, angle))
-        flipped = convert_to_np(flip_image(pil_img))
+#         # rotate and flip
+#         rotated = convert_to_np(rotate_image(pil_img, angle))
+#         flipped = convert_to_np(flip_image(pil_img))
 
-        # add to the original dataset
-        X_train_new = np.append(X_train_new, rotated.reshape(1, 784), axis = 0)
-        X_train_new = np.append(X_train_new, flipped.reshape(1, 784), axis = 0)
-        y_train_new = np.append(y_train_new, y_train[i].reshape(1,1), axis = 0)
-        y_train_new = np.append(y_train_new, y_train[i].reshape(1,1), axis = 0)
+#         # add to the original dataset
+#         X_train_new = np.append(X_train_new, rotated.reshape(1, 6400), axis = 0)
+#         X_train_new = np.append(X_train_new, flipped.reshape(1, 6400), axis = 0)
+#         y_train_new = np.append(y_train_new, y_train[i].reshape(1,1), axis = 0)
+#         y_train_new = np.append(y_train_new, y_train[i].reshape(1,1), axis = 0)
 
-        # print out progress
-        if i % 100 == 0:
-            print("Processed {i} files out of {total}.".format(i= i, total = X_train.shape[0]))
+#         # print out progress
+#         if i % 100 == 0:
+#             print("Processed {i} files out of {total}.".format(i= i, total = X_train.shape[0]))
 
-    return X_train_new, y_train_new
+#     return X_train_new, y_train_new
 
-def view_label_heatmap(X_train, y_train, label, label_name):
-    """
-    Function to plot the heatmap for images with same label.
-    INPUT:
-        X_train - (numpy array) training dataset
-        y_train - (numpy array) labels for training dataset
-        label - (int) label for images
-        label_name - (str) name for images label
+# def view_label_heatmap(X_train, y_train, label, label_name):
+#     """
+#     Function to plot the heatmap for images with same label.
+#     INPUT:
+#         X_train - (numpy array) training dataset
+#         y_train - (numpy array) labels for training dataset
+#         label - (int) label for images
+#         label_name - (str) name for images label
 
-    OUTPUT: None
-    """
-    # filter X_train to remove all other images
-    label_filter = y_train == label
-    X = pd.DataFrame(X_train)
-    X_train_labeled = X[label_filter]
+#     OUTPUT: None
+#     """
+#     # filter X_train to remove all other images
+#     label_filter = y_train == label
+#     X = pd.DataFrame(X_train)
+#     X_train_labeled = X[label_filter]
 
-    # find mean value for pixels
-    X_mean = np.sum(X, axis = 0).values
+#     # find mean value for pixels
+#     X_mean = np.sum(X, axis = 0).values
 
-    # plot image
-    fig, ax = plt.subplots(figsize=(6,9))
-    ax.set_title(label_name)
-    ax.imshow(X_mean.reshape(28, 28).squeeze())
-    ax.axis('off')
+#     # plot image
+#     fig, ax = plt.subplots(figsize=(6,9))
+#     ax.set_title(label_name)
+#     ax.imshow(X_mean.reshape(80, 80).squeeze())
+#     ax.axis('off')
 
-    plt.savefig(label_name + '.png')
+#     plt.savefig(label_name + '.png')
 
-def view_images_grid(X_train, y_train, label, label_name):
-    """
-    Function to plot grid with several examples of images with
-    passed label.
-    INPUT:
-        X_train - (numpy array) training dataset
-        y_train - (numpy array) labels for training dataset
-        label - (int) label for images
-        label_name - (str) name for images label
+# def view_images_grid(X_train, y_train, label, label_name):
+#     """
+#     Function to plot grid with several examples of images with
+#     passed label.
+#     INPUT:
+#         X_train - (numpy array) training dataset
+#         y_train - (numpy array) labels for training dataset
+#         label - (int) label for images
+#         label_name - (str) name for images label
 
-    OUTPUT: None
-    """
-    indices = np.where(y_train == label)
-    X = pd.DataFrame(X_train)
+#     OUTPUT: None
+#     """
+#     indices = np.where(y_train == label)
+#     X = pd.DataFrame(X_train)
 
-    for label_num in range(0,50):
-        plt.subplot(5,10, label_num+1) #create subplots
-        image = X.iloc[indices[0][label_num]].as_matrix().reshape(28,28)  #reshape images
-        plt.imshow(image) #plot the data
-        plt.xticks([]) #removes numbered labels on x-axis
-        plt.yticks([]) #removes numbered labels on y-axis
-        plt.suptitle(label_name)
+#     for label_num in range(0,50):
+#         plt.subplot(5,10, label_num+1) #create subplots
+#         image = X.iloc[indices[0][label_num]].as_matrix().reshape(80,80)  #reshape images
+#         plt.imshow(image) #plot the data
+#         plt.xticks([]) #removes numbered labels on x-axis
+#         plt.yticks([]) #removes numbered labels on y-axis
+#         plt.suptitle(label_name)
 
-        plt.savefig(label_name + '_grid.png')
+#         plt.savefig(label_name + '_grid.png')
 
 def plot_image(image, label_name):
     """
     Helper function to plot 1 part of animated image.
     """
-    fig, ax = plt.subplots(figsize=(5,5))
+    fig, ax = plt.subplots(figsize=(8,8))
 
     plt.imshow(image) #plot the data
     plt.xticks([]) #removes numbered labels on x-axis
@@ -296,7 +296,7 @@ def create_animated_images(X_train, y_train, label, label_name):
 
     images = []
     for label_num in range(0,50):
-        image = X.iloc[indices[0][label_num]].as_matrix().reshape(28,28)  #reshape images
+        image = X.iloc[indices[0][label_num]].as_matrix().reshape(80,80)  #reshape images
         images.append(image)
 
     # save plotted images into a gif
@@ -354,8 +354,8 @@ def crop_image(image):
     # paste to the center of a resulting image
     dst_im.paste(cropped_image, offset, cropped_image)
 
-    #resize to 28,28
-    dst_im.thumbnail((28,28), Image.ANTIALIAS)
+    #resize to 80,80
+    dst_im.thumbnail((80,80), Image.ANTIALIAS)
 
     return dst_im
 
--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,9 +1,104 @@
+import os
+import cv2
+import pandas as pd
+import numpy as np
+import ast
 
 
-# cnn.ipynb
+def apk(actual, predicted, k=3):
+    """Computes average precision at k between two lists of items"""
+    if len(predicted) > k:
+        predicted = predicted[:k]
+    score = 0.0
+    num_hits = 0.0
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+    if not actual:
+        return 0.0
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=3):
+    """Computes mean average precision at k between two lists of lists of items"""
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+
+def preds2catids(predictions):
+    return pd.DataFrame(np.argsort(-predictions, axis=1)[:, :3], columns=['a', 'b', 'c'])
+
+def f2cat(filename: str) -> str:
+    return filename.split('.')[0]
+
+def list_all_categories():
+    files = os.listdir(os.path.join(INPUT_DIR, 'train_sim_animals'))
+    return sorted([f2cat(f) for f in files], key=str.lower)
 
 
+def df_to_image_array(df, size, lw=6):
+    df['drawing'] = df['drawing'].apply(ast.literal_eval)
+    x = np.zeros((len(df), size, size))
+    for i, raw_strokes in enumerate(df.drawing.values):
+        x[i] = draw_cv2(raw_strokes, size=size, lw=lw)
+    x = x / 255.
+    x = x.reshape((len(df), size, size, 1)).astype(np.float32)
+    return x
+
+def draw_cv2(raw_strokes, size=256, lw=6, time_color=True):
+    img = np.zeros((256, 256), np.uint8)
+    for t, stroke in enumerate(raw_strokes):
+        for i in range(len(stroke[0]) - 1):
+            color = 255 - min(t, 10) * 13 if time_color else 255
+            _ = cv2.line(img, (stroke[0][i], stroke[1][i]),
+                         (stroke[0][i + 1], stroke[1][i + 1]), color, lw)
+    if size != 256:
+        return cv2.resize(img, (size, size))
+    else:
+        return img
 
 
-# mobilenet.ipynb
+# class MulticlassTruePositives(tf.keras.metrics.Metric):
+#     def __init__(self, name='multiclass_true_positives', **kwargs):
+#         super(MulticlassTruePositives, self).__init__(name=name, **kwargs)
+#         self.true_positives = self.add_weight(name='tp', initializer='zeros')
+
+#     def update_state(self, y_true, y_pred, sample_weight=None):
+#         y_pred = tf.reshape(tf.argmax(y_pred, axis=1), shape=(-1, 1))
+#         values = tf.cast(y_true, 'int32') == tf.cast(y_pred, 'int32')
+#         values = tf.cast(values, 'float32')
+#         if sample_weight is not None:
+#             sample_weight = tf.cast(sample_weight, 'float32')
+#             values = tf.multiply(values, sample_weight)
+#         self.true_positives.assign_add(tf.reduce_sum(values))
+
+#     def result(self):
+#         return self.true_positives
+
+#     def reset_states(self):
+#         # The state of the metric will be reset at the start of each epoch.
+#         self.true_positives.assign(0.)
+
+
+# class PerformanceVisualizationCallback(Callback):
+#     def __init__(self, model, validation_data, image_dir):
+#         super().__init__()
+#         self.model = model
+#         self.validation_data = validation_data
+        
+#         os.makedirs(image_dir, exist_ok=True)
+#         self.image_dir = image_dir
+
+#     def on_epoch_end(self, epoch, logs={}):
+#         y_pred = np.asarray(self.model.predict(self.validation_data[0]))
+#         y_true = self.validation_data[1]             
+#         y_pred_class = np.argmax(y_pred, axis=1)
+
+#         # plot and save confusion matrix
+#         fig, ax = plt.subplots(figsize=(16,12))
+#         plot_confusion_matrix(y_true, y_pred_class, ax=ax)
+#         fig.savefig(os.path.join(self.image_dir, f'confusion_matrix_epoch_{epoch}'))
+
+#        # plot and save roc curve
+#         fig, ax = plt.subplots(figsize=(16,12))
+#         plot_roc(y_true, y_pred, ax=ax)
+#         fig.savefig(os.path.join(self.image_dir, f'roc_curve_epoch_{epoch}'))
 
